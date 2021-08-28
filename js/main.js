2eb86e026404d96afb05f4b587a9b7dd4157248a
jyjang1222 <89000811+jyjang1222@users.noreply.github.com>
2021-08-28 17:08:06 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,8 +8,12 @@ $(function() {
     if(w >= 1197) {
       $('.mo_gnb_bg').show()
       $('#header nav').removeClass('mo_nav_on')
+      $('.depth_2').hide()
     }
-    else {$('.mo_gnb_bg').hide()}
+    else {
+      $('.mo_gnb_bg').hide()
+      $('#gnb>li>a').children('span').removeClass('mo_nav_chevron_on')
+    }
   }) // 창크기 변경후 스타일 리셋
 
   $('#header').hover(function() {
@@ -41,10 +45,12 @@ $(function() {
     }
   }) //pc_gnb
   
-  $('#gnb li>a').click(function() {
+  $('#gnb>li>a').click(function() {
     if(w < 1197) {
+      $('#gnb>li>a').not(this).children('span').removeClass('mo_nav_chevron_on')
+      $(this).children('span').toggleClass('mo_nav_chevron_on')
       $('.depth_2').stop().slideUp(300)
-      $(this).next().stop().slideToggle(300)
+      $(this).next().stop().slideToggle(300)      
     }
   }) //mo_gnb
 
@@ -55,7 +61,12 @@ $(function() {
     search = $('.search_wrap').css('display')
     if(search != "none") {
       $('.search_wrap input[type=search]').focus()
-    }    
+      $('#header').find('img').attr('src', 'img/gs_logo_on.svg')
+    }
+    else {
+      $('#header').find('img').attr('src', 'img/gs_logo_off.svg')
+      $('#header').removeClass('header_on')
+    }
   }) //검색
 
   $('.lang_area').click(function() {
@@ -64,19 +75,34 @@ $(function() {
     lang = $(this).css('overflow')
   }) //언어선택
 
+  $('.lang_area').mouseleave(function() {
+    if(lang != "hidden") {
+      $(this).removeClass('lang_area_on')
+      lang = $(this).css('overflow')
+    }
+  })
+
   $('.mo_gnb_btn').click(function() {
+    if(search != "none") {
+      $('#header').removeClass('header_on')
+      $('#header').find('img').attr('src', 'img/gs_logo_off.svg')
+      $('.search_wrap').hide();
+      $('.search_btn').removeClass('search_btn_on')
+      search = $('.search_wrap').css('display')
+    }
     $('.mo_gnb_bg').show()
     $('#header nav').addClass('mo_nav_on')
     $('.mo_nav_on').stop().animate({right: 0}, 300)
     $('.mo_gnb_gsLogo').attr('src', 'img/gs_logo_on.svg')
   }) //모바일 gnb 열기
  
-  $('.mo_gnb_close').click(function() {    
+  $('.mo_gnb_close').click(function() {
+    $('.depth_2').stop().slideUp(200)
+    $('#gnb>li>a').children('span').removeClass('mo_nav_chevron_on')
     $('.mo_nav_on').stop().animate({right: '-300px'}, 300, function() {
       $('#header nav').removeClass('mo_nav_on')
-      $('.depth_2').hide()
       $('.mo_gnb_bg').hide()
-    })   
+    })
   }) //모바일 gnb 닫기
 
   $('.manage_slider').slick({
